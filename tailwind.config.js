7ff7399b4999fdf9259d4d506e4de9f92da7f348
andreskps <pipesierra146@gmail.com>
2024-03-10 09:42:37 -0500

    update config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-module.exports = {
+export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/preline/dist/*.js",
+    './node_modules/preline/preline.js',
   'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  darkMode: 'class', // or 'media' or 'class'
   plugins: [
     require('preline/plugin'),
     require('flowbite/plugin'),
