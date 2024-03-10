886498bb7334dfad2cb6b96ea01c97edd6505248
andreskps <pipesierra146@gmail.com>
2024-03-10 09:30:13 -0500

    fix bug preline

--- src/Preline.jsx
+++ src/Preline.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
 
 export const Preline = () => {
   const location = useLocation();
 
   useEffect(() => {
-    import("preline/preline");
-  }, []);
-  useEffect(() => {
-    // @ts-ignore
-    HSStaticMethods.autoInit();
+    import("preline/preline").then((module) => {
+      module.HSAccordion.autoInit();
+      module.HSCollapse.autoInit();
+
+
+    });
   }, [location.pathname]);
+
   return <></>;
 };
